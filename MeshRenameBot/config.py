67f96cda6c66f527f91220d67dff09720af68fcb
LukasK14 <142650435+LukasK14@users.noreply.github.com>
2023-08-29 12:57:57 +0630

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://LukasK:lukask@cluster0.ekkllug.mongodb.net/cluster0?retryWrites=true&w=majority"]
+        API_HASH = [str, "3dc0142644d6cb107b9679aa20825cbc"]
+        API_ID = [int, 20480767]
+        BOT_TOKEN = [str, "6205925838:AAET0s2wKEozjs9x0jfjDBUqIP57PI1f8V8"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -17,7 +17,7 @@ except ImportError:
         # Access Restriction
         IS_PRIVATE = [bool, False]
         AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        OWNER_ID = [int, 5579935415]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
